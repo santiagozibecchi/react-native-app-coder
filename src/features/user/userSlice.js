e5ccc77285079cace3f14d6a15d7f20a3c44b987
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2024-05-11 18:11:23 -0300

    add setCameraImage to set the profile image to the user state in redux

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -19,10 +19,13 @@ export const authSlice = createSlice({
         clearUser: (state) => {
             state.value.user = null;
             state.value.token = null;
+        },
+        setCameraImage: (state, { payload }) => {
+            state.value.imageCamera = payload
         }
     }
 })
 
-export const { setUser, clearUser, setCameraImage } = authSlice.actions
+export const { setUser, clearUser, setCameraImage } = authSlice.actions;
 
-export default authSlice.reducer
+export default authSlice.reducer;
--- src/screens/profile/UserProfileScreen.jsx
+++ src/screens/profile/UserProfileScreen.jsx
@@ -1,18 +1,39 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
 import React from 'react'
 import { PrincipalLayout } from '../../components/layout/PrincipalLayout'
 import { Title } from '../../components/ui/Title'
+import { useSelector } from 'react-redux'
+import { useGetProfileImageQuery } from '../../services/profileService'
 
 export const UserProfileScreen = () => {
+  const defaultImageRoute = "../../../assets/images/defaultProfile.png"
+
+  const { imageCamera, localId } = useSelector((state) => state.auth.value);
+  const { data: profileImageFromDB } = useGetProfileImageQuery(localId);
+
+
+
   return (
     <PrincipalLayout>
 
       <Title text='Perfil' center customStyles={{ fontSize: 24 }} />
-      
+
       <View style={styles.perfilContainer}>
 
         <View style={styles.imageContainer}>
-          
+          {profileImageFromDB || imageCamera ? (
+            <Image
+              source={{ uri: profileImageFromDB?.image || imageCamera }}
+              style={styles.profileImage}
+              resizeMode="cover"
+            />
+          ) : (
+            <Image
+              source={require(defaultImageRoute)}
+              style={styles.profileImage}
+              resizeMode="cover"
+            />
+          )}
         </View>
 
         <View style={styles.configContainer}>
@@ -20,7 +41,7 @@ export const UserProfileScreen = () => {
         </View>
 
       </View>
-      
+
 
     </PrincipalLayout>
   )
@@ -29,14 +50,21 @@ export const UserProfileScreen = () => {
 const styles = StyleSheet.create({
   perfilContainer: {
     flex: 1,
-    backgroundColor: "grey",
     marginBottom: 45
   },
   imageContainer: {
     flex: 1,
-    backgroundColor: "red"
+    justifyContent: "center",
+    alignItems: "center",
+    borderBottomWidth: 0.8,
+    borderBottomColor: "grey"
   },
   configContainer: {
     flex: 1,
+  },
+  profileImage: {
+    width: "90%",
+    height: "90%",
+    borderRadius: 150
   }
 })
