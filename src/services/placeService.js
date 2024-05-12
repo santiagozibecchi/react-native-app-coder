915c857de6b88aae100e2ace6bdcda8132963c6f
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2024-05-12 13:27:36 -0300

    remove

--- src/services/placeService.js
+++ src/services/placeService.js
@@ -4,7 +4,6 @@ import { baseUrl } from "../databases/realTimeDataBase"
 export const placeAPI = createApi({
     reducerPath: "placeAPI", //Establish a unique name for the API
     baseQuery: fetchBaseQuery({ baseUrl: baseUrl }),
-    tagTypes: ['profileImageGet'], //Declare tags
     endpoints: (builder) => ({
         getCategories: builder.query({
             query: () => `categories.json`,
