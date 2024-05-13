243ef8fa1cdfd7b51242eeba63e9b7f57fd30ca7
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2024-05-12 23:28:42 -0300

    save favouritePlaceIds on DB

--- src/hooks/useFavouritePlace.jsx
+++ src/hooks/useFavouritePlace.jsx
@@ -1,19 +1,22 @@
 import { useDispatch, useSelector } from "react-redux";
 import { updateFavouritePlace } from "../features/favourite/favouriteSlice";
 import { usePostFavouritePlaceIdsMutation } from "../services/placeService";
+import { useEffect } from "react";
 
 export const useFavouritePlace = (placeId) => {
     const dispatch = useDispatch();
+    const [triggerPostFavouritePlaceIds, _] = usePostFavouritePlaceIdsMutation();
     const { favouritePlaceIds } = useSelector((state) => state.favourite.value);
     const { localId } = useSelector((state) => state.auth.value);
-
-    const [triggerPostFavouritePlaceIds, _] = usePostFavouritePlaceIdsMutation();
-  
+    
     const onTouchFavourite = () => {
         dispatch(updateFavouritePlace({ placeId }));
-        // servicio para guardar los ids en RTD
-        triggerPostFavouritePlaceIds({ favouritePlaceIds, localId });
     }
+
+    useEffect(() => {
+        triggerPostFavouritePlaceIds({ favouritePlaceIds, localId });
+    }, [favouritePlaceIds]);
+    
 
     const isAFavouritePlace = () => {
         const isFavourite = favouritePlaceIds.findIndex((favId) => favId === placeId) !== -1;
