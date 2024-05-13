f67aab355c38dfa28d29166be0451bd802660575
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2024-05-12 23:51:18 -0300

    wip on homescreen

--- src/screens/home/HomeScreen.jsx
+++ src/screens/home/HomeScreen.jsx
@@ -1,11 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import { PrincipalLayout } from '../../components/layout/PrincipalLayout'
 import { Title } from '../../components/ui/Title'
 
 export const HomeScreen = () => {
   return (
     <PrincipalLayout>
-      <Title text='Hola mundo Ahre' />
+      <Title text='Encuentra los lugares más cercanos a tu dirección' center />
+
+      
+
+
     </PrincipalLayout>
   )
 }
+
+const styles = StyleSheet.create({
+  title: {
+  }
+})
