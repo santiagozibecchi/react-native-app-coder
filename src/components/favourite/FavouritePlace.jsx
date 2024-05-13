3182dd45a715908a631aaf086e9ad8c7e1b86ca2
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2024-05-12 21:30:39 -0300

    show favourites data

--- src/components/favourite/FavouritePlace.jsx
+++ src/components/favourite/FavouritePlace.jsx
@@ -1,13 +1,70 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
+import { colors } from '../../constants/colors';
 import { useGetPlaceByIdQuery } from '../../services/placeService'
 
 export const FavouritePlace = ({ favPlaceId }) => {
-    
-    const { data: place, isLoading, isSuccess } = useGetPlaceByIdQuery(favPlaceId);
+
+  const { data: place, isLoading } = useGetPlaceByIdQuery(favPlaceId);
+
+  if (isLoading) {
+    return;
+  }
 
   return (
-    <View>
-      <Text>FavouritePlace</Text>
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={{ uri: place.images[0] }}
+        />
+      </View>
+      <View style={[styles.detailContainer, { backgroundColor: colors.primary, opacity: 0.8 }]}>
+        <View style={styles.placeContainer}>
+          <View>
+            <Text style={{ fontWeight: "bold" }}>{place.title}</Text>
+            <Text>Rating: {place.rating}</Text>
+            <Text>Ciudad: {place.city}</Text>
+            <Text>categoría: {place.category}</Text>
+          </View>
+        </View>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginVertical: 5,
+  },
+  imageContainer: {
+    borderRadius: 15,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 3,
+      height: 3,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 15,
+    elevation: 5,
+  },
+  image: {
+    borderBottomLeftRadius: 15,
+    borderTopLeftRadius: 15,
+    width: 150,
+    height: 100
+  },
+  detailContainer: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-between",
+    borderBottomRightRadius: 15,
+    borderTopRightRadius: 15,
+  },
+  placeContainer: {
+    marginTop: 5,
+    marginLeft: 5,
+  }
+})
--- src/screens/favourite/FavouriteScreen.jsx
+++ src/screens/favourite/FavouriteScreen.jsx
@@ -2,15 +2,23 @@ import { PrincipalLayout } from '../../components/layout/PrincipalLayout'
 import { useSelector } from 'react-redux'
 import { FavouritePlace } from '../../components/favourite/FavouritePlace'
 import { Title } from '../../components/ui/Title';
+import { ScrollView, StyleSheet } from 'react-native';
 
 export const FavouriteScreen = () => {
   const { favouritePlaceIds } = useSelector((state) => state.favourite.value);
 
   return (
-    <PrincipalLayout>
-      <Title text='Tus favoritos' center/>
-      {favouritePlaceIds.map(favPlaceId => <FavouritePlace key={favPlaceId} favPlaceId={ favPlaceId } />)}
-    </PrincipalLayout>
+    <ScrollView >
+      <PrincipalLayout style={{ marginBottom: 40 }}>
+        <Title text='Tus favoritos' center />
+        {favouritePlaceIds.map(favPlaceId => <FavouritePlace key={favPlaceId} favPlaceId={favPlaceId} />)}
+      </PrincipalLayout>
+    </ScrollView>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 60
+  }
+})
