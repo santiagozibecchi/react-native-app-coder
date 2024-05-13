d26fe0a214821d90f8119a187e8c675ed0824330
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2024-05-12 23:44:13 -0300

    implement the logic to update favouritePlaceIds when user like a place or when comes in for the first time

--- src/screens/favourite/FavouriteScreen.jsx
+++ src/screens/favourite/FavouriteScreen.jsx
@@ -1,16 +1,35 @@
+import { useEffect } from 'react';
 import { PrincipalLayout } from '../../components/layout/PrincipalLayout'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { FavouritePlace } from '../../components/favourite/FavouritePlace'
 import { Title } from '../../components/ui/Title';
 import { ScrollView } from 'react-native';
 import { Notice } from '../../components/ui/Notice';
 import { useNavigation } from '@react-navigation/native';
 import { PLACE_COMPONENT } from '../places/place_component';
+import { useGetFavouritePlaceIdsQuery } from '../../services/placeService';
+import { updateFavouritePlaceIds } from '../../features/favourite/favouriteSlice';
 
 export const FavouriteScreen = () => {
-  const { favouritePlaceIds } = useSelector((state) => state.favourite.value);
 
+  const dispatch = useDispatch();
   const navigate = useNavigation();
+
+  const { favouritePlaceIds } = useSelector((state) => state.favourite.value);
+  const { localId } = useSelector((state) => state.auth.value);
+  const { data: favouritePlaceIdsFromDB, isLoading } = useGetFavouritePlaceIdsQuery(localId);
+
+  useEffect(() => {
+    // Se ejecuta cuando se monta por primera vez y cuando se guarda nuevos ids en el array de
+    // con la idea de manejar un solo estado para mostrar los favoritos
+    if (!isLoading && favouritePlaceIdsFromDB) {
+      dispatch(updateFavouritePlaceIds(favouritePlaceIdsFromDB));
+    }
+  }, [isLoading, favouritePlaceIdsFromDB, dispatch]);
+
+  if (isLoading) {
+    return;
+  }
 
   return (
     <ScrollView >
