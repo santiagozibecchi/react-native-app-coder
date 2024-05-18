2830abc77b5c203770491edecc2d446a5bf9676f
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2024-05-18 17:26:25 -0300

    fix: hasMoreImagesToLoad - acces to the current value - wrap onLoadMoreImages to avoid re-renders (?)

--- src/hooks/useLoadImagesLazy.jsx
+++ src/hooks/useLoadImagesLazy.jsx
@@ -1,21 +1,22 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useCallback } from 'react';
 
 export const useLoadImagesLazy = (images) => {
     const allImagesRef = useRef(images);
     const incrementRef = useRef(4);
-    const intialImagesToLoad = images.slice(0, 4)
-    const [displayedImages, setDisplayedImages] = useState(intialImagesToLoad);
+    const initialImagesToLoad = images.slice(0, 4);
+    const [displayedImages, setDisplayedImages] = useState(initialImagesToLoad);
 
-    const onLoadMoreImages = () => {
-        incrementRef.current += 4
+    // useCallback: evitamos que esta función se recree cada vez que el estado de este hook cambie, ya que esta función es totalmente independiente del estado
+    // es lo que pienso no estoy 100% seguro porque dentro del scope de la fn esta "setDisplayedImages".... 
+    const onLoadMoreImages = useCallback(() => {
+        // El uso de useRef tiene como motivo evitar renderizados no necesarios
+        incrementRef.current += 4;
         setDisplayedImages(allImagesRef.current.slice(0, incrementRef.current));
-    }
+    }, []);
 
     return {
         displayedImages,
-        hasMoreImagesToLoad: allImagesRef.length === displayedImages.length,
-
-        // Methods
+        hasMoreImagesToLoad: !(allImagesRef.current.length === displayedImages.length),
         onLoadMoreImages,
-  }
-}
+    };
+};
