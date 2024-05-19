ccbc585bf7b9b529ab975dc471ea9eac5c0da714
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2024-05-19 17:24:02 -0300

    add a important comment to take it in the future

--- src/navigation/PlacesStackNavigator.jsx
+++ src/navigation/PlacesStackNavigator.jsx
@@ -19,6 +19,13 @@ export const PlacesStackNavigator = () => {
                 component={PlaceScreenListCategory}
                 name={PLACE_COMPONENT.list_category_screen}
             />
+            {/* 
+                PlaceDetailScreen podria ser util tenerlo solamente en un componente separado del Stack para hacerlo reutilizable.
+                Es decir, dejaria de ser una pantalla, caso contrario no lo puedo re-utilizar en la pantalla de favoritos.
+                ¿Qué es lo que pasa?
+                Si muestro este Componente/Stack para ver los detalles de la lista de favoritos, llama a todo el Stack completo, por lo que cuando quiero
+                volver atrás para ver mi lista de favoritos NO PUEDO porque el goback() en este punto es la lista de CATEGORIAS DE LUGARES (PlaceScreenListCategory)
+            */}
             <Stack.Screen
                 component={PlaceDetailScreen}
                 name={PLACE_COMPONENT.detail_screen}
